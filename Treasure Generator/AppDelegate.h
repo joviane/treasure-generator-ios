97a5c46b390100641db576c4e07982be9b5a435e
Joviane <joviane@gmail.com>
2016-03-24 17:32:26 -0300

    Initial Commit

---
+++ Treasure Generator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Treasure Generator
+//
+//  Created by Joviane Jardim on 3/24/16.
+//  Copyright © 2016 Deco Verri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
